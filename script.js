1790351c466cda5a433c8c70074aa9cd846a3657
Gleb Churkin <glebdrum@gmail.com>
2020-06-17 16:30:47 +0300

    completed lesson02

---
+++ script.js
@@ -0,0 +1,20 @@
+let money = 30000;
+let income =' 21000';
+let addExpenses ="internet, taxi, rent";
+let deposit = true;
+let mission = 30000;
+let period = 5;
+
+ 
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей ");
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(", "));
+
+
+let budgetDay = (money/30) ;
+console.log(budgetDay);
